0cf555135e79dad54821ef0edf04ece39be3c48c
Isabele <isabelebcorrea@hotmail.com>
2025-10-07 19:01:39 -0300

    Cors

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-// 🔹 Importar rotas
 const userRoutes = require('./src/routes/users');
 const storeRoutes = require('./src/routes/store');
 const supplierRoutes = require('./src/routes/supplier');
 const productRoutes = require('./src/routes/product');
 const orderRoutes = require('./src/routes/order');
+const campaignRoutes = require('./src/routes/campaign');
 
 const app = express();
 const port = 3000;
 
+app.use(cors()); 
 app.use(express.json());
 
-
-// 🔹 Configuração do Swagger
 const options = {
   definition: {
     openapi: '3.0.0',
@@ -36,26 +36,24 @@ const options = {
       },
     ],
   },
-  apis: [path.join(__dirname, 'src/routes/*.js')],
+  apis: [path.join(__dirname, '/src/routes/*.js')],
 };
 
 const swaggerDocs = swaggerJsDoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// 🔹rotas
 app.use('/api/users', userRoutes);
+app.use('/api/campaign', campaignRoutes);
 app.use('/api/suppliers', supplierRoutes);
 app.use('/api/stores', storeRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
-// Rota raiz
 app.get('/', (req, res) => {
   res.send('API da Central de Compras funcionando!');
 });
 
-// 🔹 Inicialização do servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando em: http://localhost:${port}`);
-  console.log(`Documentação Swagger: http://localhost:${port}/api-docs`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Documentação da API disponível em http://localhost:${port}/api-docs`);
 });
